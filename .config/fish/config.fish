203121814c9228d842de43659b3195c50aec223c
Tommi Pöntinen <tommipontinen76@gmail.com>
2024-05-13 15:08:19 +0300

    Fixed the swaync font issue

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -205,8 +205,6 @@ function org-search -d "send a search string to org-mode"
     printf $output
 end
 
-neofetch
-
 ### END OF FUNCTIONS ###
 
 
@@ -221,11 +219,6 @@ alias ...='cd ../..'
 alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
-
-# dnf
-alias inst="sudo dnf install"
-alias upd="sudo dnf update"
-alias rem="sudo dnf remove"
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
@@ -246,12 +239,11 @@ alias push='git push origin'
 alias tag='git tag'
 alias newtag='git tag -a'
 
-# dnf
-alias inst="sudo dnf install"
-alias upd="sudo dnf update"
-alias rem="sudo dnf remove"
+alias yay="paru"
 
 #flatpak
 alias fpupd='flatpak update'
 alias fpinst='flatpak install'
 alias fprem='flatpak remove'
+
+hyfetch
